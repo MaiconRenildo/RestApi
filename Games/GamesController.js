b4c9570d9b4de00b9d135025e94dfccd46661ec6
Maicon Renildo <maicon.renildo1@gmail.com>
2021-08-03 13:15:36 -0300

    replace

--- Games/GamesController.js
+++ Games/GamesController.js
@@ -59,6 +59,7 @@ router.delete("/game/:id",(req,res)=>{
 router.post('/game',(req,res)=>{
   let name=req.body.name;
   let price=req.body.price;
+  price=price.replace(',','.');
 
   if(name==undefined || price==undefined){
     res.sendStatus(404)
@@ -86,6 +87,7 @@ router.put("/game/:id",(req,res)=>{
   }else{
     let newName=req.body.name;
     let newPrice=req.body.price;
+    newPrice=newPrice.replace(',','.');
     let object={}
 
     if(newPrice==undefined && newName==undefined){
